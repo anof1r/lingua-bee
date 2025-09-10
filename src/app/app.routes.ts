1af817c572fd4e71907eb527df0f3f492bf4b6d1
d.anofriev <d.anofriev@yadro.com>
2025-09-10 22:09:22 +0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', redirectTo: 'dictionary', pathMatch: 'full' },
+      //   {
+      //     path: 'dictionary',
+      //     loadComponent: () => import('./pages/dictionary/dictionary.component')
+      //       .then(m => m.DictionaryComponent)
+      //   },
+      //   {
+      //     path: 'quiz',
+      //     loadComponent: () => import('./pages/quiz/quiz.component')
+      //       .then(m => m.QuizComponent)
+      //   },
+      //   {
+      //     path: 'profile',
+      //     loadComponent: () => import('./pages/profile/profile.component')
+      //       .then(m => m.ProfileComponent)
+      //   }
+    ],
+  },
+];
